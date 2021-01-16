451c714d9e71b68f293569047ee9c7a83675a280
oluwashola1100 <oluwasholatemitayo@gmail.com>
2021-01-16 11:52:59 +0100

    info updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+// import ReactPaginate from "react-paginate";
 
 import "./App.css";
 import ProfileGrid from "./components/main/ProfileGrid";
--- src/components/main/Pagination.js
+++ src/components/main/Pagination.js
@@ -1,33 +1,7 @@
 import React from "react";
 
-const Pagination = ({ recordsPerPage, totalRecords, paginate }) => {
-  const pageNumbers = [];
-  let Numbers = totalRecords / recordsPerPage;
-
-  for (let i = 1; i <= Math.ceil(Numbers); i++) {
-    pageNumbers.push(i);
-  }
-  return (
-    <ul className="pagination">
-      <li className="icon">
-        <a href="#!">
-          <i>chevron_left</i>
-        </a>
-      </li>
-      {pageNumbers.map((number) => (
-        <li key={number} style={{ marginLeft: "5px" }}>
-          <button className="btn" onClick={() => paginate(number)}>
-            {number}
-          </button>
-        </li>
-      ))}
-      <li className="icon">
-        <a href="#!">
-          <i>chevron_right</i>
-        </a>
-      </li>
-    </ul>
-  );
+const Pagination = () => {
+  return <div></div>;
 };
 
 export default Pagination;
